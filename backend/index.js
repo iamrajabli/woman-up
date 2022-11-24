45cd3e0cbb8e54d868b14955ad1f80bf97fe1ff9
Hikmat Rajabli <iamrajabli@outlook.com>
2022-11-24 19:30:25 +0400

    frontend completed

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 const corsOptions = {
